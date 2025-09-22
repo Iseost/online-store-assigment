e8a5d01a8325605c1bdb9930e844de5ce5b2d79c
Iselin Engen <iselin.osterhus@gmail.com>
2025-09-22 10:02:21 +0200

    update porduct page
    Refactor ProductPage to use custom hook for product fetching and loading state

---
+++ vite-project/src/pages/useProducts.js
@@ -0,0 +1,32 @@
+
+import { useEffect, useState } from "react";
+import { getProducts } from "../api/productsApi.jsx";
+
+export function useProducts() {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        let isMounted = true;
+
+        async function fetchData() {
+            try {
+                const data = await getProducts();
+                if (isMounted) setProducts(data);
+            } catch (err) {
+                if (isMounted) setError(err.message);
+            } finally {
+                if (isMounted) setLoading(false);
+            }
+        }
+
+        fetchData();
+
+        return () => {
+            isMounted = false;
+        };
+    }, []);
+
+    return { products, loading, error };
+}
